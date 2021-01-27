f71bf096f1db0b0dd8b9c66b48a6c824fd2a9852
akshaykrjain <akshay.kumar.jain@mimiro.no>
2021-01-27 11:28:46 +0100

    iam user

--- tf/iam/main.tf
+++ tf/iam/main.tf
@@ -1,38 +1,42 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_iam_access_key" "opa" {
+  user    = "${aws_iam_user.opa.name}"
+}
 
-resource "aws_iam_role" "test_role" {
-  name = "dev_role"
+resource "aws_iam_user" "opa" {
+  name = "datahub_opa_iam_user"
+}
 
-  assume_role_policy = <<EOF
+resource "aws_iam_user_policy" "opa_ro" {
+  name = "test"
+  user = "${aws_iam_user.opa.name}"
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
+      "Action": [
+        "s3:*"
+      ],
       "Effect": "Allow",
-      "Sid": ""
+      "Resource": "arn:aws:s3:::mimiro-*/*"
     }
   ]
 }
 EOF
+}
 
-  tags = {
-    tag-key = "tag-value"
-  }
+output "opa_iam_user_secret" {
+  sensitive = true
+  value = "${aws_iam_access_key.opa.secret}"
+}
+
+output "opa_iam_user_access_key" {
+  sensitive = true
+  value = "${aws_iam_access_key.opa.id}"
 }
